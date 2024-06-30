9273dbca63d2bb6ef2cbaeb0bdead091620b171e
여인철 <yic@brobotics.kr>
2024-06-30 17:03:02 +0900

    first commit

---
+++ src/main/java/com/practice/awspracrtice/AwsPracrticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.awspracrtice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPracrticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsPracrticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/awspracrtice/AwsPracrticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.awspracrtice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPracrticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
